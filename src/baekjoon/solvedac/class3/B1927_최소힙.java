afe8efa9a1703a717d1a1b633ae2f9e66d30906a
Kim Donghyun <eastmon94@naver.com>
2021-01-21 20:14:47 +0900

    필요없는 import 제거

--- src/baekjoon/solvedac/class3/B1927_최소힙.java
+++ src/baekjoon/solvedac/class3/B1927_최소힙.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class B1927_최소힙 {
     public static void main(String[] args) throws Exception{

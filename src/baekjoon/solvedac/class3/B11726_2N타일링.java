2d6d9472b2be8eb2b69a00bb2d05164a5023012c
Kim Donghyun <eastmon94@naver.com>
2021-02-04 21:52:39 +0900

    백준 2*N타일링 문제풀이

---
+++ src/baekjoon/solvedac/class3/B11726_2N타일링.java
@@ -0,0 +1,25 @@
+package baekjoon.solvedac.class3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B11726_2N타일링 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(in.readLine());
+        
+        if(N==1) {
+            System.out.println(1);
+            return;
+        }
+        int[] memo = new int[N+1];
+        memo[1]=1;
+        memo[2]=2;
+        for(int i=3; i<=N; i++) {
+            memo[i] = (memo[i-2] + memo[i-1])%10007;
+        }
+
+        System.out.println(memo[N]);
+    }
+}

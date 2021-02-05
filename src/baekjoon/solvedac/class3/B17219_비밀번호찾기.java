abd3062e034c9b6500d1a61924c9c07579c69ac3
Kim Donghyun <eastmon94@naver.com>
2021-02-05 16:35:22 +0900

    백준 비밀번호찾기 문제풀이

---
+++ src/baekjoon/solvedac/class3/B17219_비밀번호찾기.java
@@ -0,0 +1,28 @@
+package baekjoon.solvedac.class3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class B17219_비밀번호찾기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> map = new HashMap<>();
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(in.readLine(), " ");
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<M; i++) {
+            sb.append(map.get(in.readLine())+"\n");
+        }
+        System.out.println(sb.toString());
+    }
+}

7b11c5a279c7aa50dd492b75cb44a7ac887e8994
Kim Donghyun <eastmon94@naver.com>
2021-02-19 14:45:25 +0900

    후위표기식 수정

--- src/baekjoon/solvedac/class4/B1918_후위표기식.java
+++ src/baekjoon/solvedac/class4/B1918_후위표기식.java
@@ -6,15 +6,12 @@ import java.util.HashMap;
 import java.util.Stack;
 
 public class B1918_후위표기식 {
-    static Stack<Character> operator;
-    static String input;
-    static HashMap<Character, Integer> m;
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        input = in.readLine();
+        String input = in.readLine();
 
-        operator = new Stack<>();
-        m = new HashMap<>();
+        Stack<Character> operator = new Stack<>();
+        HashMap<Character, Integer> m = new HashMap<>();
         m.put('+', 1);m.put('-', 1);m.put('*', 2);m.put('/', 2);m.put('(', 0);
         StringBuilder answer = new StringBuilder();
 

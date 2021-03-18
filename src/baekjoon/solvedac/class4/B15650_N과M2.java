5051f1c9b69206dd77287782333c3e03204c7640
Kim Donghyun <eastmon94@naver.com>
2021-03-18 18:55:41 +0900

    백준 N과 M(2) 문제풀이

---
+++ src/baekjoon/solvedac/class4/B15650_N과M2.java
@@ -0,0 +1,39 @@
+package baekjoon.solvedac.class4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B15650_N과M2 {
+    static int N, M;
+    static int[] numbers;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[M];
+        
+        sb = new StringBuilder();
+        combination(0, 1);
+
+        System.out.print(sb.toString());
+    }
+
+    static void combination(int idx, int cur) {
+        if(idx==M) {
+            for(int i=0; i<M; i++) {
+                sb.append(numbers[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=cur; i<=N; i++) {
+            numbers[idx] = i;
+            combination(idx+1, i+1);
+        }
+    }
+}
